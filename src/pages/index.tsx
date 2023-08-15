9827f8cdfa97f2acced9a55a78827e22e69f6d4f
andrianfaa <hi@anfa.my.id>
2023-08-15 07:58:09 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="container p-4 m-4 md:p-6">
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default Home;
